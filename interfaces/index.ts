0d705bfbc2e5a4174d96f16e435172e36a85ae0d
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 19:33:38 +0100

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,24 @@
+export interface Review {
+  name: string;
+  avatar: string;
+  rating: number;
+  comment: string;
+}
+
+export interface Address {
+  city: string;
+  country: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  rating: number;
+  address: Address;
+  image: string;
+  images?: string[];
+  description: string;
+  category: string[];
+  price: number;
+  reviews: Review[];
+}
 
