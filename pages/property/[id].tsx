ad9407e6a6454ecf10895762124c87ca330b5a08
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-30 19:34:30 +0100

    Update [id].tsx

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,1 +1,18 @@
+import { useRouter } from "next/router";
+import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
+import PropertyDetail from "@/components/property/PropertyDetail";
+
+export default function PropertyPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
+
+  if (!property) return <p>Property not found</p>;
+
+  return (
+    <div className="container mx-auto p-6">
+      <PropertyDetail property={property} />
+    </div>
+  );
+}
 
